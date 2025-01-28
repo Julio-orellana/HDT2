baf0238d21b73511ab9dd4177485b536c22c9869
Julio <julioes134@outlook.es>
2025-01-28 15:45:56 -0600

    Testing configuration and refactor

--- src/main/java/uvg/hdt2/Main.java
+++ src/main/java/uvg/hdt2/Main.java
@@ -1,7 +1,7 @@
-package org.example;
+package uvg.hdt2;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Testing First con figuration of the project");
     }
 }
