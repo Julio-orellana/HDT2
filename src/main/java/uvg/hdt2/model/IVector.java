c3fa58594e9bbe12d971b430c4e7369deb0e9655
Julio <julioes134@outlook.es>
2025-01-28 15:44:55 -0600

    Vector Interface added

--- src/main/java/uvg/hdt2/model/IVector.java
+++ src/main/java/uvg/hdt2/model/IVector.java
@@ -1,4 +1,12 @@
 package uvg.hdt2.model;
 
-public interface IVector {
+public interface IVector<T> {
+
+    void add(T element);
+    T removeLast();
+    T getLast();
+    int size();
+    boolean isEmpty();
+    void clear();
+
 }
