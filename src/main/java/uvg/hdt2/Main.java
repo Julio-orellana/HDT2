dfd0cd7c57386b5dfde77e5f61fac441ee07ca10
Julio <julioes134@outlook.es>
2025-01-29 12:55:21 -0600

    Testing classes

--- src/main/java/uvg/hdt2/Main.java
+++ src/main/java/uvg/hdt2/Main.java
@@ -1,7 +1,15 @@
 package uvg.hdt2;
 
+import uvg.hdt2.model.PostfixCalculator;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Testing First con figuration of the project");
+        PostfixCalculator calculator = new PostfixCalculator();
+        try {
+             calculator.readFromFile("src/main/resources/data.txt");
+             String rs = calculator.getResult();
+                System.out.println(rs);
+        } catch (Exception e) {
+            e.printStackTrace();
     }
-}
+} }
