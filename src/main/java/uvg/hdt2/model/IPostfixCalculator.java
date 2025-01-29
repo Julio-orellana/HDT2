6fbdceee170e7e440447d5b297c676572c246064
Julio <julioes134@outlook.es>
2025-01-29 11:13:38 -0600

    Postfix Calc Interface

---
+++ src/main/java/uvg/hdt2/model/IPostfixCalculator.java
@@ -0,0 +1,10 @@
+package uvg.hdt2.model;
+
+import java.io.IOException;
+
+public interface IPostfixCalculator {
+
+    void readFromFile(String path) throws IOException;
+    double evaluateExpression() throws IllegalArgumentException;
+
+}
